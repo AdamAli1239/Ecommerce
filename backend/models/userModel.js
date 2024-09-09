80bfc208ece354403d1d842c24e536d34434e65a
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-09 18:50:58 -0400

    created my user model and created 2 functions and the schema for the auth

---
+++ backend/models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require('jsonwebtoken')
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.signup = async function (email, password) {
+  if (!validator.isEmail(email)) {
+    throw Error("Enter a proper email please.");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Not a strong enough password.");
+  }
+  const user = await this.findOne({ email });
+  if (user) {
+    throw Error("This email is already in use.");
+  }
+
+  const salt = 10;
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const createdUser = await this.create({ email, hashedPassword });
+  // maybe I should say password:hashedPasswoed for this create?
+  // we will see soon enough though
+  return createdUser;
+};
+
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({email})
+    if(user){
+        const checkPasswords = await bcrypt.compare(password,user.password)
+        if(checkPasswords){
+            // do something
+        }
+        throw Error('Passwords do not match.')
+    }
+    throw Error('No user found with that email.')
+};
+
+module.exports = mongoose.model("userAuth", userSchema);
