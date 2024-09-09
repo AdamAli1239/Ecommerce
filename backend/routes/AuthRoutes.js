a20666e6d5ca7cdda2f8c0cc9750450b3e9f0293
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-09 18:51:12 -0400

    created the basic auth routes

---
+++ backend/routes/AuthRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const router = express.Router()
+// for the user to create an account
+router.post('/signup',{})
+
+//for the user to log into their account
+router.post('/login',{})
