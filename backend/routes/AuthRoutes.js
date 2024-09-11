fe00ab2593e83b5d53501eb5c2ae1bf2b8543dd0
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-11 12:06:59 -0400

    created the routes file for authenticating users

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,8 +1,10 @@
-const express = require('express')
-
-const router = express.Router()
+const express = require("express");
+const { signUser, logUserIn } = require("../controllers/userController");
+const router = express.Router();
 // for the user to create an account
-router.post('/signup',{})
+router.post("/signup", signUser);
 
 //for the user to log into their account
-router.post('/login',{})
+router.post("/login", logUserIn);
+
+module.exports = router;
