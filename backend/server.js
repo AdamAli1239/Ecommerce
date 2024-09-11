472cd98a255e82e483718294932ea6debc9c6598
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-11 12:08:32 -0400

    added another route, connected to the database and added middleware functions to help deconstructing req and parsing cookies

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const ItemRoutes = require("./routes/ItemRoutes");
-
+const AuthRoutes = require("./routes/AuthRoutes");
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
 });
-
+app.use("/user", AuthRoutes);
 app.use("/items", ItemRoutes);
 
 app.listen(process.env.PORT);
+mongoose.connect(process.env.DB_URL);
