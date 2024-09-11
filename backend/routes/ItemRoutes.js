d6a390f0f25fd16082165de4ac8b14eb2a007275
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-11 12:07:20 -0400

    created the routes for the various items the user will be able to view

--- backend/routes/ItemRoutes.js
+++ backend/routes/ItemRoutes.js
@@ -1,4 +1,20 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
+// GET all the tops available
+router.get("/tops", () => {});
 
+// GET all the bottoms available
+router.get("/bottoms", () => {});
+// GET all the sweaters available
+router.get("/sweaters", () => {});
+// GET all the pants available
+router.get("/pants", () => {});
+// GET all the t-shirts available
+router.get("/t-shirts", () => {});
+// GET all the shorts available
+router.get("/shorts", () => {});
+// GET all the socks available
+router.get("/socks", () => {});
+
+module.exports = router;
