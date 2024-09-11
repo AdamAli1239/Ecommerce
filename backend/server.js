b520cf51a932365c6fb5c6f302efdefe5b755057
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-11 12:06:46 -0400

    added some routing and some console logging to help with debugging

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
+const ItemRoutes = require("./routes/ItemRoutes");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
+app.use("/items", ItemRoutes);
 
 app.listen(process.env.PORT);
