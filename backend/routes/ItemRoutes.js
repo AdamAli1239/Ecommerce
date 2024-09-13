cad05e655d808d071711b9ebfef96b8f272b3d0c
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-13 12:08:34 -0400

    imported in the controller functions for the products/items

--- backend/routes/ItemRoutes.js
+++ backend/routes/ItemRoutes.js
@@ -1,20 +1,29 @@
 const express = require("express");
-
+const {
+  createItem,
+  getTops,
+  getBottoms,
+  getSweaters,
+  getPants,
+  getTshirts,
+  getShorts,
+  getSocks,
+} = require("../controllers/productController");
 const router = express.Router();
 // GET all the tops available
-router.get("/tops", () => {});
+router.get("/tops", getTops);
 
 // GET all the bottoms available
-router.get("/bottoms", () => {});
+router.get("/bottoms", getBottoms);
 // GET all the sweaters available
-router.get("/sweaters", () => {});
+router.get("/sweaters", getSweaters);
 // GET all the pants available
-router.get("/pants", () => {});
+router.get("/pants", getPants);
 // GET all the t-shirts available
-router.get("/t-shirts", () => {});
+router.get("/t-shirts", getTshirts);
 // GET all the shorts available
-router.get("/shorts", () => {});
+router.get("/shorts", getShorts);
 // GET all the socks available
-router.get("/socks", () => {});
-
+router.get("/socks", getSocks);
+router.post('/create',createItem)
 module.exports = router;
