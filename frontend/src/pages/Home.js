21439f97cc8d6ecebe029c7a40ff4165afb77037
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-10-01 23:54:54 -0400

    created a basic home page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import PantsComponent from "../components/PantsComponent";
+import SockComponent from "../components/SockComponent";
+import SweaterComponent from "../components/SweatersComponent";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>hi</h2>
+      <PantsComponent />
+      <SweaterComponent />
+      <SockComponent />
+    </div>
+  );
+};
+
+export default Home;
